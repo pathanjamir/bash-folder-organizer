0c02dd818d6f737eb5d0249f6d967c79ef39cd37
Pathan Jamir <130206048+pathanjamir@users.noreply.github.com>
2024-01-13 21:33:20 +0530

    Add files via upload

---
+++ organize_folders.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Function to create date folder
+create_date_folder() {
+    today=$(date +"%d_%b_%Y")
+    mkdir "$today"
+    echo "Folder $today created."
+}
+
+# Function to create month and year folder
+create_month_year_folder() {
+    current_month_year=$(date +'%b_%Y')
+    if [ ! -d "$current_month_year" ]; then
+        mkdir "$current_month_year"
+        echo "Folder $current_month_year created."
+    else
+        echo "Folder $current_month_year already exists."
+    fi
+}
+
+# Desktop/support
+cd Desktop/support || exit
+create_month_year_folder
+cd "$current_month_year" || exit
+create_date_folder
+cd
+
+# Downloads
+cd Downloads || exit
+create_month_year_folder
+cd "$current_month_year" || exit
+create_date_folder
+cd
+
+# Pictures
+cd Pictures || exit
+create_month_year_folder
+cd "$current_month_year" || exit
+create_date_folder
+cd
+
+# Documents
+cd Documents || exit
+create_month_year_folder
+cd "$current_month_year" || exit
+create_date_folder
+cd
+
+# Desktop/txt
+cd Desktop/txt || exit
+create_month_year_folder
+cd "$current_month_year" || exit
+create_date_folder
+cd
